875134c9b0c5c1c55822544aa9b0360a23909975
Dimitris Theodorou <dimitris.theodorou@gmail.com>
2015-05-31 13:41:16 +0200

    style

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Flask-SQLAlchemy-Session
 -----------------------
@@ -18,27 +19,28 @@ from setuptools import setup
 if os.path.abspath(__file__).split(os.path.sep)[1] == 'vagrant':
     del os.link
 
-setup(name="Flask-SQLAlchemy-Session",
-      version="1.0",
-      packages=["flask_sqlalchemy_session"],
-      author="Dimitris Theodorou",
-      author_email="dimitris.theodorou@gmail.com",
-      url='http://github.com/dtheodor/flask-sqlalchemy-session',
-      license="MIT",
-      description='SQL Alchemy session scoped on Flask requests.',
-      long_description=__doc__,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Environment :: Web Environment',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-          'Topic :: Software Development :: Libraries :: Python Modules'
-      ],
-      install_requires=["sqlalchemy>=0.9", "Flask>=0.9", "Werkzeug>=0.6.1"],
-      tests_require=["pytest>=2.6", "mock>=1.0"],
-      extras_require={
-          'docs': ["Sphinx>=1.2.3", "alabaster>=0.6.3"]
-      }
+setup(
+    name="Flask-SQLAlchemy-Session",
+    version="1.0",
+    packages=["flask_sqlalchemy_session"],
+    author="Dimitris Theodorou",
+    author_email="dimitris.theodorou@gmail.com",
+    url='http://github.com/dtheodor/flask-sqlalchemy-session',
+    license="MIT",
+    description='SQL Alchemy session scoped on Flask requests.',
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=["sqlalchemy>=0.9", "Flask>=0.9", "Werkzeug>=0.6.1"],
+    tests_require=["pytest>=2.6", "mock>=1.0"],
+    extras_require={
+        'docs': ["Sphinx>=1.2.3", "alabaster>=0.6.3"]
+    }
 )
